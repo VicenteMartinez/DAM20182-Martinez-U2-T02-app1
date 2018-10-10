21f37709f01886c1e1a1e2aad00449e1b3fabba5
vicenteMZ <vimartinezza@ittepic.edu.mx>
2018-10-10 14:28:07 -0600

    T02 practica 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,12 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-
+import { OtherPage } from '../pages/otherPage/otherPage';
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    OtherPage
   ],
   imports: [
     BrowserModule,
@@ -19,12 +20,13 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    OtherPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    { provide: ErrorHandler, useClass: IonicErrorHandler }
   ]
 })
-export class AppModule {}
+export class AppModule { }
